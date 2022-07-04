0713944f406568f10849c2882409887bcb04db8f
Federico Liborio <federico-l-@hotmail.com>
2022-07-03 21:22:02 -0300

    Se suben primeros avances sobre las vistas y templates

--- sistemadecompra/views.py
+++ sistemadecompra/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Producto
+from django.template import loader
+
 
 # Create your views here.
+
+def catalogo(request):
+    lista_productos = Producto.objects.all()
+
+    template = loader.get_template('sistemadecompra/catalogo.html')
+    context = {
+        'lista_productos': lista_productos,
+    }
+    return render(request, 'sistemadecompra/catalogo.html', context)
